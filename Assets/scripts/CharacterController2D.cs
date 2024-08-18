e43b40ad23dc0ca2da8ae7892d7f48f868ba0038
Anthony Blount <anthony00.blount00@gmail.com>
2024-08-18 14:10:53 -0500

    puff and unpuff, particles and sound

--- Assets/scripts/CharacterController2D.cs
+++ Assets/scripts/CharacterController2D.cs
@@ -8,9 +8,19 @@ public class CharacterController2D : MonoBehaviour
     private bool move = false;
     private Vector2 moveVec;
     private Rigidbody2D rb;
-    public float acceleration;
-    public float speedCap;
+    public float unpuffedAcceleration;
+    public float unpuffedSpeedCap;
+    public float puffedAcceleration;
+    public float puffedSpeedCap;
+    private bool puffed = false;
+    public GameObject unpuffedVisuals;
+    public GameObject puffedVisuals;
     public Vector2 lastCheckpointPos;
+    private CapsuleCollider2D unpuffedCollider;
+    private CircleCollider2D puffedCollider;
+    public float timeToUnpuff;
+    public GameObject puffParticlePrefab;
+    public AudioSource puffSound;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +30,9 @@ public class CharacterController2D : MonoBehaviour
         // so we can keep the fish enabled in the editor but prevent control until play is pressed
         gameObject.SetActive(false);
         lastCheckpointPos = rb.position;
+        unpuffedCollider = gameObject.GetComponent<CapsuleCollider2D>();
+        puffedCollider = gameObject.GetComponent<CircleCollider2D>();
+        SetPuffed(false);
     }
 
     // Update is called once per frame
@@ -32,6 +45,10 @@ public class CharacterController2D : MonoBehaviour
     {
         if (move)
         {
+
+            float acceleration = puffed ? puffedAcceleration : unpuffedAcceleration;
+            float speedCap = puffed ? puffedSpeedCap : unpuffedSpeedCap;
+            
             rb.AddForce(moveVec * acceleration, ForceMode2D.Force);
 
             float clampedVerticalSpeed = Mathf.Clamp(rb.velocity.y, -speedCap, speedCap);
@@ -54,6 +71,25 @@ public class CharacterController2D : MonoBehaviour
     void OnFire(InputValue value)
     {
         Debug.Log("fire");
+        SetPuffed(true);
+        Invoke("Unpuff", timeToUnpuff);
+    }
+
+    void SetPuffed(bool puffed)
+    {
+        this.puffed = puffed;
+        unpuffedVisuals.SetActive(!puffed);
+        puffedVisuals.SetActive(puffed);
+        unpuffedCollider.enabled = !puffed;
+        puffedCollider.enabled = puffed;
+        Instantiate(puffParticlePrefab, rb.position, transform.rotation);
+        puffSound.time = 0.1f;
+        puffSound.Play();
+    }
+
+    void Unpuff()
+    {
+        SetPuffed(false);
     }
 
     public void Die()
