39fcb81e537a3f89ecfe7f23b83c5e3f6799be0e
Anthony Blount <anthony00.blount00@gmail.com>
2024-08-18 23:15:15 -0500

    torpedos collide with the ground now

--- Assets/Torpedo.cs
+++ Assets/Torpedo.cs
@@ -13,13 +13,24 @@ public class Torpedo : MonoBehaviour
     void Start()
     {
         spawnTime = Time.time;
-        target = FindObjectsByType<CharacterController2D>(FindObjectsSortMode.None)[0].GetComponent<Rigidbody2D>();
+        var search = FindObjectsByType<CharacterController2D>(FindObjectsSortMode.None);
+        if (search.Length > 0)
+        {
+            target = search[0].GetComponent<Rigidbody2D>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!target)
+        {
+            var search = FindObjectsByType<CharacterController2D>(FindObjectsSortMode.None);
+            if (search.Length > 0)
+            {
+                target = search[0].GetComponent<Rigidbody2D>();
+            }
+        }
     }
 
     void FixedUpdate()
@@ -29,14 +40,17 @@ public class Torpedo : MonoBehaviour
             Die();
         }
 
-        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
-        Vector2 moveVec = (target.position - pos);
-        moveVec.Normalize();
-        transform.eulerAngles = (new Vector3(0, 0, Vector2.Angle(Vector2.up, moveVec) * (moveVec.x < 0 ? 1 : -1)));
+        if (target)
+        {
+            Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+            Vector2 moveVec = (target.position - pos);
+            moveVec.Normalize();
+            transform.eulerAngles = (new Vector3(0, 0, Vector2.Angle(Vector2.up, moveVec) * (moveVec.x < 0 ? 1 : -1)));
 
-        // should probably be using a rigidbody but oh well
-        // would be nice to have the interpolation though
-        transform.position = Vector2.MoveTowards(pos, moveVec + pos, .1f * speed);
+            // should probably be using a rigidbody but oh well
+            // would be nice to have the interpolation though
+            transform.position = Vector2.MoveTowards(pos, moveVec + pos, .1f * speed);
+        }
     }
 
     void Die()
@@ -49,12 +63,20 @@ public class Torpedo : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    // private void OnTriggerEnter2D(Collider2D other) {
+    //     CharacterController2D player = other.gameObject.GetComponent<CharacterController2D>();
+    //     if (player)
+    //     {
+    //         player.Die();
+    //         Die();
+    //     }
+    // }
+    private void OnCollisionEnter2D(Collision2D other) {
         CharacterController2D player = other.gameObject.GetComponent<CharacterController2D>();
         if (player)
         {
             player.Die();
-            Die();
         }
+        Die();
     }
 }
